3adeb81a741a3e3aebd6df11744c22882d06699c
Natalya <natalya_peixoto@hotmail.com>
2018-11-07 16:14:43 -0200

    npm

---
+++ README.md
@@ -0,0 +1,18 @@
+ # Boilerplate NPM 
+
+ Boilerplate simples para porjetos em JS e SCSS. 
+
+ ### Dependencias do projeto 
+ - npm (^6.4.1)
+
+ ### Como montar o projeto
+
+ ```  bash
+ npm install && npm run build 
+ ```
+
+ ### Como assistir os arquivos JS e CSS 
+
+ ```bash 
+ npm run watch 
+ ```
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  }, 
+  module: {
+    rules: [{
+        test: /\.scss$/,
+        use: [{
+            loader: "style-loader"
+        }, {
+            loader: "css-loader"
+        }, {
+            loader: "sass-loader",
+            options: {
+                includePaths: ["absolute/path/a", "absolute/path/b"]
+            }
+        }]
+    }]
+  }
+};
+
+
